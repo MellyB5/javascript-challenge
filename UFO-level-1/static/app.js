37f7130aa4f3856b9c6b5ae0ec3b686d695d9b06
Mel Benson <nat_mel@hotmail.com>
2021-09-16 18:28:13 +0800

    updated

--- UFO-level-1/static/app.js
+++ UFO-level-1/static/app.js
@@ -4,7 +4,9 @@ var tableData = data;
 // console.log(tableData);
 var tbody = d3.select("tbody");
 console.log(tbody);
+
 /*
+data information and headings
 city: "springfield"
 comments: "black floating object with lights"
 country: "us"
@@ -18,22 +20,35 @@ function draw_data(updated_data){
     updated_data.forEach(element => {
         // console.log(element);
         var tableRow= tbody.append("tr");
-        tbody.append("td").text(element.datetime);
-        tbody.append("td").text(element.city);
-        tbody.append("td").text(element.state);
-        tbody.append("td").text(element.country);
-        tbody.append("td").text(element.shape);
-        tbody.append("td").text(element.durationMinutes);
-        tbody.append("td").text(element.comments);
+        tableRow.append("td").text(element.datetime);
+        tableRow.append("td").text(element.city);
+        tableRow.append("td").text(element.state);
+        tableRow.append("td").text(element.country);
+        tableRow.append("td").text(element.shape);
+        tableRow.append("td").text(element.durationMinutes);
+        tableRow.append("td").text(element.comments);
     });
-    
 }
+
 draw_data(tableData);
+
 function update_page(){
-    var value = d3.select("#datetime").property("value");
-    console.log(value);
-    var new_data = tableData.filter(d => d["datetime"]===value);
-    console.log(new_data);
-    draw_data(new_data);
+    var value1 = d3.select("#datetime").property("value");
+    // console.log(value);
+    var new_data1 = tableData.filter(d => d.datetime===value1);
+    // console.log(new_data);
+    draw_data(new_data1);
+    // var value2 = d3.select("#city").property("value");
+    // var new_data2 = tableData.filter(d => d.city===value2)
+    // draw_data(new_data2);
 }
-d3.select("#datetime").on("change", update_page);
+d3.select("#filters").on("change", update_page);
+
+
+// trial code
+// function runEnter(){
+//     d3.event.preventDefault();
+//     var inputElement = d3.select("#filters");
+//     var InputValue = inputElement.property("value");
+// 
+// }
