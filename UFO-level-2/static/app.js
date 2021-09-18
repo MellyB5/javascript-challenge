9a10ab5b09a160e0621e369165c4249b6b78cb8d
Mel Benson <nat_mel@hotmail.com>
2021-09-18 15:22:06 +0800

    cleaned

--- UFO-level-2/static/app.js
+++ UFO-level-2/static/app.js
@@ -1,24 +1,12 @@
-// from data.js
 var tableData = data;
-// YOUR CODE HERE!
-// console.log(tableData);
+
 var tbody = d3.select("tbody");
 console.log(tbody);
 
-/*
-data information and headings
-city: "springfield"
-comments: "black floating object with lights"
-country: "us"
-datetime: "1/12/2010"
-durationMinutes: "3 minutes"
-shape: "changing"
-state: "or"
-*/
+
 function draw_data(updated_data){
     tbody.html("");
     updated_data.forEach(element => {
-        // console.log(element);
         var tableRow= tbody.append("tr");
         tableRow.append("td").text(element.datetime);
         tableRow.append("td").text(element.city);
